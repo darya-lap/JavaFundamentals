15a808c221aea1234ea7d443cb3668b3cc9641d5
Darya <daryalaptsevich@gmail.com>
2016-09-04 11:30:44 +0300

    All tasks

---
+++ intro/src/main/java/Task_1/javase01/t01/logic/Logic.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Дарья on 04.09.2016.
+ */
+package javase01.t01.logic;
+
+public class Logic {
+    public String method(){
+        return "I am string in Logic.";
+    }
+
+}
+
---
+++ intro/src/main/java/Task_1/javase01/t01/main/Main1.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Дарья on 04.09.2016.
+ */
+package javase01.t01.main;
+
+import javase01.t01.logic.Logic;
+
+public class Main1 {
+
+    public static void main(String[] args) {
+        Logic logic = new Logic();
+        System.out.println(logic.method());
+    }
+}
+
